61c68f462ac0729a56a289133b0dcff1b1bcf180
Seymen Kalkan <seymenkalkan@gmail.com>
2023-04-23 23:20:01 +0300

    Update main function
    - Added implementation testing for Alternative and Attribute

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "Alternative.hpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    Attribute *referenceAttribute = new Attribute[3];
+    referenceAttribute[0] = Attribute(0.200, "Yükseklik", false);
+    referenceAttribute[1] = Attribute(0.300, "Genişlik", false);
+    referenceAttribute[2] = Attribute(0.500, "Derinlik", true);
+
+    Alternative *alternative = new Alternative("Kalem Kutusu", 3, referenceAttribute);
+    std::cout << *alternative << std::endl;
+
     return 0;
 }
